c668d278ca8706627f8866647a50a8ec69c2f62a
fadlidwicandra15 <fadlidwicandra@gmail.com>
2025-01-13 03:56:34 -0800

    Delete modul 5 &  tugas directory

--- modul 5 &  tugas/11-LatihanPercabangan/Main.py
+++
@@ -1,8 +0,0 @@
-totalbelanja = float(input("masukkan total belanja Rp. "))
-if totalbelanja > 500000 :
-   # aksi 1
-   print("Selamat dapat bonus mouse dan keyboard")
-else :
-   # aksi 2
-   print("dapat gantungan kunci")
-print('Terima Kasih')
